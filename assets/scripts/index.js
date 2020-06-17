2e3e66f4922e1ef4a808e625b0008cd55e0b80bf
Nils <haberkamp.n@gmail.com>
2020-06-17 18:57:16 +0200

    added mobile nav

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,9 @@
 import NavigationToggler from './modules/NavigationToggler'
 
 // eslint-disable-next-line no-unused-vars
-const mainNavToggler = new NavigationToggler('#toggle', '#nav', 'is--openned')
+const mainNavToggler = new NavigationToggler(
+	'#toggle',
+	'#nav',
+	'fly-in',
+	'fly-out'
+)
--- assets/scripts/modules/NavigationToggler.js
+++ assets/scripts/modules/NavigationToggler.js
@@ -3,12 +3,15 @@ export default class NavigationToggler {
 	 * Toggles state of navigation when the toggle element is clicked
 	 * @param {string} toggleElement CSS query wich points to the toggle element
 	 * @param {string} navigationElement CSS query wich points to the navigation element
-	 * @param {string} toggleClass Class that toggles the state of the navigation
+	 * @param {string} toggleClass Class that toggles the state of the navigation.
+	 * 	 If collapseClass is set this, this class will be used for opening the nav
+	 * @param {string} collapseClass Class that will get set to class attribute of navigation.
 	 */
-	constructor(toggleElement, navigationElement, toggleClass) {
+	constructor(toggleElement, navigationElement, toggleClass, collapseClass) {
 		this.toggleElement = document.querySelector(toggleElement)
 		this.navigationElement = document.querySelector(navigationElement)
 		this.toggleClass = toggleClass
+		this.collapseClass = collapseClass
 
 		// automatically add event listener
 		this.addListener()
@@ -28,20 +31,34 @@ export default class NavigationToggler {
 
 	expandNavigation() {
 		const initialClassAttribute = this.navigationElement.getAttribute('class')
-		const newClassAttribute = initialClassAttribute.concat(
-			' ',
-			this.toggleClass
-		)
+
+		// add toggle/expand class
+		let newClassAttribute = initialClassAttribute.concat(' ', this.toggleClass)
+
+		if (
+			typeof this.collapseClass === 'string' &&
+			this.collapseClass.length !== 0
+		) {
+			// remove collapse class
+			newClassAttribute = newClassAttribute.replace(this.collapseClass, '')
+		}
 
 		this.navigationElement.setAttribute('class', newClassAttribute)
 	}
 
 	collapseNavigation() {
 		const initialClassAttribute = this.navigationElement.getAttribute('class')
-		const newClassAttribute = initialClassAttribute.replace(
-			this.toggleClass,
-			''
-		)
+
+		// remove toggle/expand class
+		let newClassAttribute = initialClassAttribute.replace(this.toggleClass, '')
+
+		if (
+			typeof this.collapseClass === 'string' &&
+			this.collapseClass.length !== 0
+		) {
+			// add collapse class
+			newClassAttribute = newClassAttribute.concat(' ', this.collapseClass)
+		}
 
 		this.navigationElement.setAttribute('class', newClassAttribute)
 	}
