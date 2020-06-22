25a5ea860652c9220807aaf344704b14424d831b
Nils <haberkamp.n@gmail.com>
2020-06-22 20:54:28 +0200

    add post card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+/**
+ * Variabless
+ */
+
+const colors = {
+	lightGrey: '#f2f2f2',
+	lightGreen: '#cce6e7',
+	darkGreen: '#529c9c',
+}
+
 module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				'light-grey': '#f2f2f2',
+				'light-grey': colors.lightGrey,
+				'accent-light': colors.lightGreen,
+				accent: colors.darkGreen,
+			},
+			textColor: {
+				'light-grey': colors.lightGrey,
+				'accent-light': colors.lightGreen,
+				accent: colors.darkGreen,
+			},
+			borderColor: {
+				'light-grey': colors.lightGrey,
+				'accent-light': colors.lightGreen,
+				accent: colors.darkGreen,
 			},
 		},
 	},
